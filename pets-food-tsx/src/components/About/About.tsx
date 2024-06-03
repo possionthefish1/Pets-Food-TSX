c00964b8b6b2c1e46e531caeb34204f3fd73d546
possionthefish1 <92135698+possionthefish1@users.noreply.github.com>
2024-06-03 21:51:43 +0700

    Ready fix-cat-img

--- pets-food-tsx/src/components/About/About.tsx
+++ pets-food-tsx/src/components/About/About.tsx
@@ -40,7 +40,7 @@ function BigAbout() {
     <div className='mx-auto hidden w-[75%] grid-cols-2 gap-24 md:grid lg:h-[600px] md:gap-16 md:p-4'>
       <div className='overflow-hidden rounded-lg'>
         <img
-          className='w-[350px] overflow-hidden rounded-lg object-[0_-150px] transition-all duration-300 ease-in-out md:w-full hover:scale-[115%] lg:hover:scale-[120%] lg:scale-[115%]'
+          className='w-[350px] overflow-hidden rounded-lg lg:object-[0_-150px] transition-all duration-300 ease-in-out md:w-full hover:scale-[115%] lg:hover:scale-[120%] lg:scale-[115%]'
           src='https://images.unsplash.com/photo-1561948955-570b270e7c36?q=80&w=2701&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
           alt='Cat Face'
         />
