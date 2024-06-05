8ae4628c33d7f9e987448c24ed33d64d6bc32791
SurawitK <sk.helloworld@gmail.com>
2024-06-06 00:02:42 +0700

    TestChakra

--- pets-food-tsx/src/App.tsx
+++ pets-food-tsx/src/App.tsx
@@ -2,6 +2,7 @@ import { About } from './components/About';
 import { Gallery } from './components/Gallery';
 import { Hero } from './components/Hero';
 import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Gallery />
       <About />
+      <Footer />
     </>
   );
 }
