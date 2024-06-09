c00a83074aeca239df6008cc4ec62ec1424fc7a6
SurawitK <sk.helloworld@gmail.com>
2024-06-09 21:56:26 +0700

    Refractor Footer

--- pets-food-tsx/src/components/Footer/Footer.tsx
+++ pets-food-tsx/src/components/Footer/Footer.tsx
@@ -1,6 +1,37 @@
+const lists = [
+  {
+    title: 'LINK',
+    contents: [
+      { text: 'Home', link: '/' },
+      { text: 'Product', link: '/' },
+      { text: 'About', link: '/' },
+      { text: 'Contact', link: '/' },
+    ],
+  },
+  {
+    title: 'INTERESTS',
+    contents: [
+      { text: 'Link1', link: '/' },
+      { text: 'Link2', link: '/' },
+      { text: 'Link3', link: '/' },
+      { text: 'Link4', link: '/' },
+    ],
+  },
+  {
+    title: 'CONTACT',
+    link: '/',
+    contents: [
+      { text: 'Dog Food', link: '/' },
+      { text: 'Bangkok', link: '/' },
+      { text: 'dog_food@gmail.com', link: '/' },
+      { text: '12345678', link: '/' },
+    ],
+  },
+];
+
 function Footer() {
   return (
-    <footer className='py-8 bg-slate-200 md:py-24'>
+    <footer className='bg-slate-200 py-8 md:py-24'>
       <div className='container mx-auto'>
         <div className='mx-auto grid w-[75%] grid-flow-row gap-8 md:grid-cols-[0.75fr_0.45fr_0.75fr_0.45fr]'>
           <div>
@@ -12,65 +43,20 @@ function Footer() {
               formulated for your dog.
             </p>
           </div>
-          <div>
-            <h5 className='pb-3 font-bold text-xl uppercase'>
-              LINK
-            </h5>
-            <ul className='grid gap-y-2 text-slate-600'>
-              <li>
-                <a href='/'>Home</a>
-              </li>
-              <li>
-                <a href='/'>Products</a>
-              </li>
-              <li>
-                <a href='/'>About</a>
-              </li>
-              <li>
-                <a href='/'>Contact</a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h5 className='pb-3 font-bold text-xl uppercase'>
-              Interests
-            </h5>
-            <ul className='grid gap-y-2 text-slate-600'>
-              <li>
-                <a href='/'>Link1</a>
-              </li>
-              <li>
-                <a href='/'>Link2</a>
-              </li>
-              <li>
-                <a href='/'>Link3</a>
-              </li>
-              <li>
-                <a href='/'>Link4</a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h5 className='pb-3 font-bold text-xl uppercase'>
-              Contact
-            </h5>
-            <ul className='grid gap-y-2 text-slate-600'>
-              <li>
-                <a href='/'>Dog Food</a>
-              </li>
-              <li>
-                <a href='/'>Bankok</a>
-              </li>
-              <li>
-                <a href='mailto: dog_food@gmail.com'>
-                  dog_food@gmail.com
-                </a>
-              </li>
-              <li>
-                <a href='tel:+6612345678'>12345678</a>
-              </li>
-            </ul>
-          </div>
+          {lists.map((list) => (
+            <div key={list.title}>
+              <h5 className='pb-3 font-bold text-xl uppercase'>
+                {list.title}
+              </h5>
+              <ul className='grid gap-y-2 text-slate-600'>
+                {list.contents.map((content) => (
+                  <li key={content.text}>
+                    <a href={list.link}>{content.text}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       </div>
     </footer>
