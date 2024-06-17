d26d9475b0f5abd6bdb1c965e0c905431aa90486
possionthefish1 <92135698+possionthefish1@users.noreply.github.com>
2024-06-18 01:45:21 +0700

    b4 sleep

--- pets-food-tsx/src/routes/product.lazy.tsx
+++ pets-food-tsx/src/routes/product.lazy.tsx
@@ -17,87 +17,168 @@ export const Route = createLazyFileRoute('/product')({
   component: Product,
 });
 
+type Product = {
+  id: number;
+  src: string;
+  price: number;
+  title: string;
+  content?: string;
+};
+
+const products: Product[] = [
+  {
+    id: 1,
+    src: 'dog/dog_food_1.jpeg',
+    price: 34.99,
+    title: 'dog food chicken flavor',
+  },
+  {
+    id: 2,
+    src: 'dog/dog_food_2.jpeg',
+    price: 34.99,
+    title: 'dog food beef flavor',
+  },
+  {
+    id: 3,
+    src: 'dog/dog_food_3.jpeg',
+    price: 34.99,
+    title: 'dog food pork liver flavor',
+  },
+  {
+    id: 7,
+    src: 'cat/cat_snack_7.jpeg',
+    price: 34.99,
+    title: 'dog food pork liver flavor',
+  },
+  {
+    id: 8,
+    src: 'cat/cat_snack_1.jpeg',
+    price: 34.99,
+    title: 'dog food pork liver flavor',
+  },
+  {
+    id: 9,
+    src: 'cat/cat_snack_4.jpeg',
+    price: 34.99,
+    title: 'dog food pork liver flavor',
+  },
+  {
+    id: 10,
+    src: 'cat/cat_toy_1.jpeg',
+    price: 34.99,
+    title: 'dog food pork liver flavor',
+  },
+  {
+    id: 11,
+    src: 'cat/cat_toy_2.jpeg',
+    price: 34.99,
+    title: 'dog food pork liver flavor',
+  },
+  {
+    id: 12,
+    src: 'dog/dog_toy_1.jpeg',
+    price: 34.99,
+    title: 'dog food pork liver flavor',
+  },
+  {
+    id: 13,
+    src: 'dog/dog_toy_3.jpeg',
+    price: 34.99,
+    title: 'dog food pork liver flavor',
+  },
+];
+
 function Product() {
   const { isOpen, onToggle } = useDisclosure();
 
   return (
     <section className='container mx-auto pt-20'>
-      <Card
-        variant='elevated'
-        maxW='sm'
-        onMouseEnter={onToggle}
-        onMouseLeave={onToggle}
+      <Stack
+        wrap={'wrap'}
+        justify={'space-between'}
+        spacing={4}
+        direction='row'
       >
-        <CardBody>
-          <Collapse in={isOpen}>
-            <Box
-              bg='white'
-              // className='bg-rose-400'
-              pb={isOpen ? '2' : '0'}
-              mt='2'
-              rounded='md'
-              shadow='md'
-            >
-              <Select
-                display='inline-block'
-                w={'20%'}
-                variant='outline'
-                borderRadius={'none'}
-                borderTopLeftRadius='10px'
-                borderBottomLeftRadius='10px'
-              >
-                {Array.from({ length: 10 }, (_x, i) => (
-                  <option
-                    key={`option${i + 1}`}
-                    value={i + 1}
-                  >
-                    {i + 1}
-                  </option>
-                ))}
-              </Select>
-
-              <Button
-                color='white'
-                fontSize='xl'
-                bg='#fb7185'
-                display='inline-block'
-                borderRadius={'none'}
-                w={'80%'}
-                _hover={{ bg: '#fda4af' }}
-                borderTopRightRadius='10px'
-                borderBottomRightRadius='10px'
-              >
-                Add to Cart
-              </Button>
-            </Box>
-          </Collapse>
-          <Image
-            src='/dog/dog_food_1.jpeg'
-            alt='Green double couch with wooden legs'
-            borderRadius='lg'
-          />
-          <Stack
-            mt='6'
-            spacing='3'
+        {products.map((product) => (
+          <Card
+            key={product.id}
+            variant='elevated'
+            maxW='sm'
+            onMouseEnter={onToggle}
+            onMouseLeave={onToggle}
           >
-            <Heading
-              as='h3'
-              fontSize='xl'
-              className='text-amber-800'
-            >
-              $34
-            </Heading>
-            <Heading
-              size='md'
-              as='h2'
-              fontSize='2xl'
-              textTransform={'capitalize'}
-            >
-              dog food chicken flavor
-            </Heading>
-          </Stack>
-        </CardBody>
-      </Card>
+            <CardBody>
+              <Collapse in={isOpen}>
+                <Box
+                  bg='white'
+                  // className='bg-rose-400'
+                  pb={isOpen ? '2' : '0'}
+                  mt='2'
+                  rounded='md'
+                  shadow='md'
+                >
+                  <Select
+                    display='inline-block'
+                    w={'20%'}
+                    variant='outline'
+                    borderRadius={'none'}
+                    borderTopLeftRadius='10px'
+                    borderBottomLeftRadius='10px'
+                  >
+                    {Array.from({ length: 10 }, (_x, i) => (
+                      <option
+                        key={`option${i + 1}`}
+                        value={i + 1}
+                      >
+                        {i + 1}
+                      </option>
+                    ))}
+                  </Select>
+
+                  <Button
+                    color='white'
+                    fontSize='xl'
+                    bg='#fb7185'
+                    display='inline-block'
+                    borderRadius={'none'}
+                    w={'80%'}
+                    _hover={{ bg: '#fda4af' }}
+                    borderTopRightRadius='10px'
+                    borderBottomRightRadius='10px'
+                  >
+                    Add to Cart
+                  </Button>
+                </Box>
+              </Collapse>
+              <Image
+                src={product.src}
+                alt='Green double couch with wooden legs'
+                borderRadius='lg'
+              />
+              <Stack
+                mt='6'
+                spacing='3'
+              >
+                <Heading
+                  as='h3'
+                  fontSize='xl'
+                  className='text-amber-800'
+                >
+                  ${product.price}
+                </Heading>
+                <Heading
+                  size='md'
+                  as='h2'
+                  fontSize='2xl'
+                  textTransform={'capitalize'}
+                >
+                  {product.title}
+                </Heading>
+              </Stack>
+            </CardBody>
+          </Card>
+        ))}
+      </Stack>
     </section>
   );
 }
