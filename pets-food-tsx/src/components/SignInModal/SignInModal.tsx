66386fd582dc3f298b71b5a0634133cd55619151
possionthefish1 <92135698+possionthefish1@users.noreply.github.com>
2024-06-26 22:51:05 +0700

    SignInModal submit working now

--- pets-food-tsx/src/components/SignInModal/SignInModal.tsx
+++ pets-food-tsx/src/components/SignInModal/SignInModal.tsx
@@ -1,10 +1,5 @@
 import {
   Button,
-  FormControl,
-  FormErrorMessage,
-  FormHelperText,
-  FormLabel,
-  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -14,28 +9,28 @@ import {
   ModalOverlay,
   useDisclosure,
 } from '@chakra-ui/react';
+import { type SubmitHandler, useForm } from 'react-hook-form';
 
-import { useState } from 'react';
-import type React from 'react';
 function SignInModal() {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const [input, setInput] = useState('');
+  type Inputs = {
+    email: string;
+    password: string;
+  };
 
-  const handleInputChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
-  ) => setInput(e.target.value);
-  const isError = input === '';
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
 
-  // function validateName(value: string) {
-  //   let error = '';
-  //   if (!value) {
-  //     error = 'Name is required';
-  //   } else if (value.toLowerCase() !== 'naruto') {
-  //     error = "Jeez! You're not a fan 😱";
-  //   }
-  //   return error;
-  // }
+  const onSubmit: SubmitHandler<Inputs> = (data) =>
+    console.log('Form Submit', data);
+
+  console.log(watch('email'));
+  console.log(watch('password'));
 
   return (
     <>
@@ -64,36 +59,31 @@ function SignInModal() {
           <ModalHeader>Create your account</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
-            {/* <FormControl isInvalid={isError}> */}
-            <FormControl>
-              <FormLabel>Email</FormLabel>
-              <Input
-                type='email'
-                value={input}
-                onChange={handleInputChange}
+            <form onSubmit={handleSubmit(onSubmit)}>
+              {/* register your input into the hook by invoking the "register" function */}
+              <input
+                defaultValue='test'
+                {...register('email')}
               />
-              {isError ? (
-                <FormErrorMessage>
-                  Email is required.
-                </FormErrorMessage>
-              ) : (
-                <FormHelperText>
-                  Enter the email you'd like to receive the
-                  newsletter on.
-                </FormHelperText>
-              )}
-            </FormControl>
-          </ModalBody>
 
-          <ModalFooter>
-            <Button
-              colorScheme='blue'
-              mr={3}
-            >
-              Save
-            </Button>
-            <Button onClick={onClose}>Cancel</Button>
-          </ModalFooter>
+              {/* include validation with required or other standard HTML validation rules */}
+              <input
+                {...register('password', { required: true })}
+              />
+              {/* errors will return when field validation fails  */}
+              {errors.password && (
+                <span>This field is required</span>
+              )}
+              <Button
+                colorScheme='blue'
+                mr={3}
+                type='submit'
+              >
+                Submit
+              </Button>
+              <Button onClick={onClose}>Cancel</Button>
+            </form>
+          </ModalBody>
         </ModalContent>
       </Modal>
     </>
