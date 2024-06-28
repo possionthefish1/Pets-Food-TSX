ed1fce8807b80c8467c354272b4f54bcb1a41d7b
SurawitK <sk.helloworld@gmail.com>
2024-06-29 00:18:19 +0700

    Next.js Navbar

--- pets-food-next/tailwind.config.ts
+++ pets-food-next/tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        lemon: ['var(--font-lemon)'],
+        mono: ['var(--font-ubuntu-mono)'],
+        bree: ['var(--font-bree-serif)'],
+
+      }
     },
   },
   plugins: [],
