f6f3d29406f5a6b1290fdd4d28e095f0be79b92c
possionthefish1 <92135698+possionthefish1@users.noreply.github.com>
2024-07-02 22:30:35 +0700

    tw-fluid

--- pets-food-next/tailwind.config.ts
+++ pets-food-next/tailwind.config.ts
@@ -1,26 +1,36 @@
-import type { Config } from "tailwindcss";
+import fluid, { extract, fontSize, screens } from 'fluid-tailwind';
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: {
+    files: [
+      './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    extract,
+  },
   theme: {
+    /** @type {import('fluid-tailwind').FluidThemeConfig} */
+    fluid: ({ theme }) => ({
+      defaultScreens: ['20rem', theme('screens.lg')],
+    }),
+    screens,
+    fontSize,
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial':
+          'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       fontFamily: {
         lemon: ['var(--font-lemon)'],
         mono: ['var(--font-ubuntu-mono)'],
         bree: ['var(--font-bree-serif)'],
-
-      }
+      },
     },
   },
-  plugins: [],
+  plugins: [fluid],
 };
 export default config;
