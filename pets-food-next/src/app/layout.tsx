a2d420c7988e2d16b6018476e4568ae6a081e4f0
possionthefish1 <92135698+possionthefish1@users.noreply.github.com>
2024-07-07 00:21:48 +0700

    Add antialised

--- pets-food-next/src/app/layout.tsx
+++ pets-food-next/src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth antialiased">
       <head>
       <body className={`${ubuntuMono.variable} ${bree.variable} ${lemon.variable}`}> 
         <Providers>
